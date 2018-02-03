51e67e58af5c17bc516f3e95d25cf0825051cb6c
Qixian Li <liqixian19970305@gmail.com>
2018-02-03 12:42:29 -0800

    Initial Commit

---
+++ LiveClassroom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveClassroom
+//
+//  Created by Tony Li on 2/3/18.
+//  Copyright © 2018 Tony Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
