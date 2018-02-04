466fe01ece8e63c502c1b35bdc81c412af05cde5
Qixian Li <liqixian19970305@gmail.com>
2018-02-03 17:51:27 -0800

    Added view controllers

---
+++ LiveClassroom/KeyCenter.swift
@@ -0,0 +1,11 @@
+//
+//  KeyCenter.swift
+//  OpenLive
+//
+//  Created by GongYuhua on 6/25/16.
+//  Copyright © 2016 Agora. All rights reserved.
+//
+
+struct KeyCenter {
+    static let AppId: String = "152468df806d413c99184320ffb1ad10"
+}
